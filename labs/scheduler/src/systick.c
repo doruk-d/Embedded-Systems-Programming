efe691b1cdcad6e058cb9e92ed74315b8f466213
doruk-d <dorukanakat@gmail.com>
2026-05-20 23:51:53 +0200

    perf(scheduler): instrument wfi entry and exit with dwt_cyccnt reads

--- labs/scheduler/src/systick.c
+++ labs/scheduler/src/systick.c
@@ -1,5 +1,6 @@
 #include "systick.h"
 #include "scheduler.h"
+#include "dwt.h"
 #include <stdint.h>
 
 #define STK_CTRL (*(volatile uint32_t *)0xE000E010)
@@ -10,6 +11,8 @@
 #define SYSTICK_FREQ 1000
 #define RELOAD_VAL ((CPU_FREQ / SYSTICK_FREQ) - 1)
 
+volatile uint32_t wfi_latency_end;
+
 void systick_init(void){
     STK_LOAD = RELOAD_VAL;
 
@@ -19,6 +22,12 @@ void systick_init(void){
     
 }
 
-void SysTick_Handler(void){
-    scheduler_run();
+__attribute__((naked)) void SysTick_Handler(void){
+    __asm__ volatile(
+        "ldr r1, =DWT_CYCCNT_ADDR   \n"
+        "ldr r1, [r1]               \n"
+        "ldr r0, =wfi_latency_end   \n"
+        "str r1, [r0]               \n"
+        "b scheduler_run            \n"
+        );
 }
