9a04f5c9e2589998415db2d883b82739b56f359a
doruk-d <dorukanakat@gmail.com>
2026-05-28 01:07:19 +0200

    test(uart): update smoke test for new uart interface

--- labs/tests/test_uart/test_uart.c
+++ labs/tests/test_uart/test_uart.c
@@ -2,14 +2,18 @@
 #include <stdint.h>
 
 int main(void){
-    if (uart_init(9600) != UART_OK)
-        return 0;
+    if (uart_init(115200) != UART_OK)
+        goto exit_main;
 
-    char input_buffer[128];
+    uart_printf("enter the input: \r\n");
+
+    int c;
     while (1){
-        // test calls 
-        uart_gets(input_buffer, 128);
-        uart_puts(input_buffer);
+        if ((c = uart_getc())== -1)
+            continue;
+        uart_putc((char)c);
     }
 
+exit_main:
+    while(1);
 }
